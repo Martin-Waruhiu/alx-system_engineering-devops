3cb5ad5e728d91ef96195f6b2fb0310fc2d9ff3b
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-05-03 18:11:18 -0700

    pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 # puppet code to kill a process in shell
 
-exec { 'killmenow':
-command  => 'pkill killmenow',
-provider => 'shell'
-onlyif   => 'pgrep killmenow',
+exec { 'pkill -f killmenow':
+  path  => 'usr/bin/:/usr/local/bin/:/bin/'
 }
