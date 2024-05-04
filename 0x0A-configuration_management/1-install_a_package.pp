8d51e20cc665f1694047c1639fe9c8bfc55214c1
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-05-03 17:33:07 -0700

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # puppet code to install flask using pip3
 
-package {'Python-pip':
-        ensure => installed
-}
-
 package {'flask':
-        ensure => '2.1.0',
-        provider => 'pip3',
-        require => ['python3-pip']
+ensure   => '2.1.0',
+provider => 'pip3',
 }
