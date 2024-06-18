a65e5e44a23b5a2aab049d470bd0ac8fa1dbeef2
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-06-18 02:34:32 -0700

    error

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
 
-"""Using what you did in the task #0, extend your Python script to export data in the Csv format"""
+
+"""
+Using what you did in the task #0, extend your Python script to export data in the CSV format.
+Requirements:
+Records all tasks that are owned by this employee
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
+
 
 import csv
 import requests
 import sys
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
